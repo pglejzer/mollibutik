d921006e07447cc8ada18cf727dd0ae3c407a194
Piotr Glejzer <glejzerpiotr@gmail.com>
2018-04-24 14:59:08 +0200

    dla przykladu
    dla przykladu

---
+++ script.js
@@ -0,0 +1,46 @@
+/*Nav*/
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+/*Slider1*/
+$('#carousel-example-generic').carousel({
+    interval: 3000,
+    pause: null
+  });
+
+/*Smooth scroll*/
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
+
+/*Hamburger*/
+
+ $(document).ready(function() {
+    $('.navbar-collapse a').click(function(){ 
+      $('.navbar-collapse').css('height', '0');
+      $('.navbar-collapse').removeClass('in');
+    });  
+ });
+
+
+function initMap() {
+        var uluru = {lat: 52.1689738, lng: 22.2675806};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
